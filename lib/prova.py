f1b887763830aa9f92cd1ecc64b87ecb4f59f3ac
lillo97 <liviobasile97@gmail.com>
2024-11-09 17:59:45 +0100

    updated gps data sending

--- lib/prova.py
+++ lib/prova.py
@@ -31,14 +31,15 @@ def convert_longitude(lon, direction):
             return decimal_lon if direction == 'E' else -decimal_lon
     return None
 
+
 def parse_nmea_sentences(nmea_sentences):
     """Parses a list of NMEA sentences and aggregates useful information."""
     data = {}
 
-    for nmea_sentence in nmea_sentences:
-        parts = nmea_sentence.split(',')
+    parts = nmea_sentences.split(',')
+    for nmea_sentence in parts:
 
-        if parts[0] == '$GNGGA':  # Global Positioning System Fix Data
+        if nmea_sentence.startswith('$GNGGA'):  # Global Positioning System Fix Data
             data['fix_time'] = parts[1]
             data['latitude'] = convert_latitude(parts[2], parts[3])  # Convert latitude
             data['longitude'] = convert_longitude(parts[4], parts[5])  # Convert longitude
@@ -48,7 +49,7 @@ def parse_nmea_sentences(nmea_sentences):
             data['altitude'] = parts[9]
             data['altitude_units'] = parts[10]
 
-        elif parts[0] == '$GNRMC':  # Recommended Minimum Specific GPS Data
+        elif nmea_sentence.startswith('$GNRMC'):  # Recommended Minimum Specific GPS Data
             data['fix_time'] = parts[1]
             data['status'] = parts[2]
             data['latitude'] = convert_latitude(parts[3], parts[4])  # Convert latitude
@@ -57,13 +58,13 @@ def parse_nmea_sentences(nmea_sentences):
             data['course_over_ground'] = parts[8]
             data['date'] = parts[9]
 
-        elif parts[0] == '$GNVTG':  # Course over ground and Ground speed
+        elif nmea_sentence.startswith('$GNVTG'):  # Course over ground and Ground speed
             data['course_over_ground'] = parts[1]
             data['speed_knots'] = parts[5]
             data['speed_kmh'] = parts[7]
 
-    #print(data)
     return data
+
 
 def print_gps_data(data):
     """Prints GPS data in a formatted way."""
@@ -83,13 +84,13 @@ def send_to_flask(data):
     print(data)
     url = 'http://127.0.0.1:8080/update_coordinates'  # Replace with your actual Flask server URL
     response = requests.post(url, json=data)
-    if response.status_code == 200:
-        print("Data successfully sent to Flask!")
-    else:
-        print(f"Failed to send data: {response.status_code}")
+    # if response.status_code == 200:
+    #     print("Data successfully sent to Flask!")
+    # else:
+    #     print(f"Failed to send data: {response.status_code}")
 
 
-serial_port = '/dev/cu.usbserial-1420'
+serial_port = '/dev/cu.usbserial-1410'
 """Reads and returns GPS data from the NEO-M8N module in an infinite loop."""
 """Reads and displays GPS information from the NEO-M8N module."""
 try:
@@ -98,37 +99,45 @@ try:
         print("Listening for GPS data...")
 
         # Create a dictionary to hold aggregated GPS data
-        aggregated_data = {}
-
-        buffer = []
+        # aggregated_data = {}
+        #
+        # buffer = []
         index = 0
         while True:
             try:
                 # Read a line of data from the GPS module
                 line = ser.readline().decode('ascii', errors='replace').strip()
+                print('line: ',line)
                 if line.startswith('$'):
-                    buffer.append(line)
-
-                    # Check if we have at least 3 sentences to parse
-                    if len(buffer) >= 3:
-                        # Parse the accumulated NMEA sentences
-                        data = parse_nmea_sentences(buffer)
-
-                        buffer.clear()  # Clear buffer for the next batch of sentences
-
-                        # Merge the data into the aggregated data dictionary
-                        aggregated_data.update(data)
-
-                        # Print the aggregated GPS data
+                    data = parse_nmea_sentences(line)
+                    print(len(data))
+                    if len(data) > 0:
                         current_time = time.time()
 
-                        # Format the time to HH:MM
-                        formatted_time = datetime.fromtimestamp(current_time).strftime('%H:%M:%S')
-                        aggregated_data['time'] = formatted_time
-                        aggregated_data['index'] = index
-                        if aggregated_data:
-                            print_gps_data(aggregated_data)
-                            send_to_flask(aggregated_data)
+                    # Format the time to HH:MM
+                    formatted_time = datetime.fromtimestamp(current_time).strftime('%H:%M:%S')
+                    data['time'] = formatted_time
+                    data['index'] = index
+                    print_gps_data(data)
+                    send_to_flask(data)
+                    #print(data)
+                    # buffer.append(line)
+                    #
+                    # # Check if we have at least 3 sentences to parse
+                    # if len(buffer) >= 3:
+                    #     # Parse the accumulated NMEA sentences
+                    #     data = parse_nmea_sentences(buffer)
+                    #
+                    #     buffer.clear()  # Clear buffer for the next batch of sentences
+                    #
+                    #     # Merge the data into the aggregated data dictionary
+                    #     aggregated_data.update(data)
+
+                    # Print the aggregated GPS data
+
+                    # if aggregated_data:
+                    #     print_gps_data(aggregated_data)
+                    #     send_to_flask(aggregated_data)
 
                 # Wait a bit before reading the next line
                 time.sleep(0.5)
