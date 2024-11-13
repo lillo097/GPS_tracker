a72fd491466d8e24d69f2a0ed277442f024676f8
lillo097 <liviobasile97@gmail.com>
2024-11-13 01:27:38 +0100

    updated latest working version from rasp

--- lib/port_finder.py
+++ lib/port_finder.py
@@ -5,6 +5,7 @@ ports = serial.tools.list_ports.comports()
 list_of_ports = []
 for port in ports:
     list_of_ports.append(port.device)
+print(list_of_ports)
 serial_port = list_of_ports[-1]
 
 print(serial_port)
